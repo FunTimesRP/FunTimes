411dbc123f1a02f71d6e24eb99e4cff62816f0b2
FunTimesRP <37424473+FunTimesRP@users.noreply.github.com>
2018-03-15 20:43:14 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,22 +8,25 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === '$ip') {
     	message.reply('server ip: 198.27.68.8:32294');
-    }
+  	}
 });
 
 client.on('message', message => {
     if (message.content === '$banner') {
     	message.reply('https://cdn.discordapp.com/attachments/292016130535587840/423597590651600896/Fun-Times-Banner-2.gif');
+  	}
 });
     
 client.on('message', message => {
     if (message.content === '$hello') {
     	message.reply('Welcome to Fun Times I am the Owner's Assistant! If you need help use command $help');
+  	}
 });
     
 client.on('message', message => {
     if (message.content === '$help') {
     	message.reply('List Of Commands: $banner, $ip, and $hello');
+  	}
 });
     
 // THIS  MUST  BE  THIS  WAY
