887dcd7cbc5b0aec9880fd4dbde24302aa6d5980
FunTimesRP <37424473+FunTimesRP@users.noreply.github.com>
2018-03-15 21:25:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '$ip') {
-    	message.channel.send('server ip: 198.27.68.8:32294');
+    	message.reply('server ip: 198.27.68.8:32294');
   	}
 });
 
@@ -20,22 +20,15 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '$hello') {
-    	message.channel.send('Hi Welcome to Fun Times I am the Owners Assistant! If you need help use command $help');
+    	message.reply('Hi Welcome to Fun Times I am the Owners Assistant! If you need help use command $help');
   	}
 });
 
 client.on('message', message => {
     if (message.content === '$help') {
-    	message.channel.send('List Of Commands: $banner, $ip, and $hello');
+    	message.reply('List Of Commands: $banner, $ip, and $hello');
   	}
 });
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
-
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
