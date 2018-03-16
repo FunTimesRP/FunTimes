2ae5933b9b702b789d75b2387e6c24f0b2c39da6
FunTimesRP <37424473+FunTimesRP@users.noreply.github.com>
2018-03-15 20:16:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,39 @@ client.on('message', message => {
   	}
 });
 
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace MyBot.Modules
+{
+    public class Test : ModuleBase<SocketCommandContext>
+    {
+        [Command("ip")]
+        public async Task wdwaDfawd()
+        {
+            await Context.Channel.SendMessageAsync("@everyone server ip: 198.27.68.8:32294");
+        }
+
+        [Command("banner")]
+        public async Task wdwaDfawdd()
+        {
+            await Context.Channel.SendMessageAsync("https://cdn.discordapp.com/attachments/292016130535587840/423597590651600896/Fun-Times-Banner-2.gif");
+        }
+
+        [Command("hello")]
+        public async Task wdwaDfawdddd()
+        {
+            await Context.Channel.SendMessageAsync("Welcome to Fun Times I am the Owner's Assistant! If you need help use command $help");
+        }
+
+        [Command("help")]
+        public async Task wdwaDfawddddd()
+        {
+            await Context.Channel.SendMessageAsync("List Of Commands: $banner, $ip, and $hello");
+        }
+    }
+}
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
