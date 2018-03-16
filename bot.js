1afef1ba77cb2255f11fb8f1c382a031cb267e72
FunTimesRP <37424473+FunTimesRP@users.noreply.github.com>
2018-03-15 21:04:18 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,31 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("ready", () => {
-    console.log("I am ready!");
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-client.on("message", message => {
-    if (message.content === "$ip") {
-    	message.reply("server ip: 198.27.68.8:32294");
+client.on('message', message => {
+    if (message.content === '$ip') {
+    	message.reply('server ip: 198.27.68.8:32294');
   	}
 });
 
-client.on("message", message => {
-    if (message.content === "$banner") {
-    	message.reply("https://cdn.discordapp.com/attachments/292016130535587840/423597590651600896/Fun-Times-Banner-2.gif");
+client.on('message', message => {
+    if (message.content === '$banner') {
+    	message.reply('https://cdn.discordapp.com/attachments/292016130535587840/423597590651600896/Fun-Times-Banner-2.gif');
   	}
 });
 
-client.on("message", message => {
-    if (message.content === "$help") {
-    	message.reply("List Of Commands: $banner, $ip, and $hello");
+client.on('message', message => {
+    if (message.content === '$help') {
+    	message.reply('List Of Commands: $banner, $ip, and $hello');
   	}
 });
 
-client.on("message", message => {
-    if (message.content === "$hello".) {
-    	message.reply("Welcome to Fun Times I am the Owner's Assistant! If you need help use command $help");
+client.on('message', message => {
+    if (message.content === '$hello'.) {
+    	message.reply('Welcome to Fun Times I am the Owners Assistant! If you need help use command $help');
   	}
 });
     
