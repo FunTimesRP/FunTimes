ca71515142871ac431ff68e047fb32fc0588eff0
FunTimesRP <37424473+FunTimesRP@users.noreply.github.com>
2018-03-15 20:56:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,24 +2,30 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log("I am ready!");
 });
 
 client.on('message', message => {
     if (message.content === '$ip') {
-    	message.reply('server ip: 198.27.68.8:32294');
+    	message.reply("server ip: 198.27.68.8:32294");
   	}
 });
 
 client.on('message', message => {
     if (message.content === '$banner') {
-    	message.reply('https://cdn.discordapp.com/attachments/292016130535587840/423597590651600896/Fun-Times-Banner-2.gif');
+    	message.reply("https://cdn.discordapp.com/attachments/292016130535587840/423597590651600896/Fun-Times-Banner-2.gif");
   	}
 });
 
 client.on('message', message => {
     if (message.content === '$help') {
-    	message.reply('List Of Commands: $banner, $ip, and $hello');
+    	message.reply("List Of Commands: $banner, $ip, and $hello");
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '$hello') {
+    	message.reply("Welcome to Fun Times I am the Owner's Assistant! If you need help use command $help");
   	}
 });
     
