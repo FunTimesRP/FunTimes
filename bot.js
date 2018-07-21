ebcfc923680715494e07787ae373338a79ed2990
FunTimesRP <37424473+FunTimesRP@users.noreply.github.com>
2018-07-21 15:01:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '$ip') {
-    	message.reply('server ip: 198.27.68.8:32294');
+    	message.reply('server ip: 185.249.196.20:32186');
   	}
 });
 
@@ -19,19 +19,19 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '$hello') {
-    	message.reply('Hi Welcome to Fun Times I am the Owners Assistant! If you need help use command $help');
+    	message.reply('Hi Welcome to Project San Andreas I am the Owners Assistant! If you need help use command $help');
   	}
 });
 
 client.on('message', message => {
     if (message.content === '$help') {
-    	message.reply('List Of Commands: $banner, $ip, $backgroundinfo, and $hello');
+    	message.reply('List Of Commands: $ip, $backgroundinfo, and $hello');
   	}
 });
 
 client.on('message', message => {
     if (message.content === '$backgroundinfo') {
-    	message.reply('Fun Times is a RP server for Five M! The Owner is Sam W. | 2B-22 AKA NiceGuy. The owner started Fun Times in 2018, and has been working on the server every since. Fun Times is a small community, but we are a very strong community when brought together! We help everyone learn how to RP and make people feel at home! We give everyone a chance on our community. We are the community of forgiveness. We can not help that some people do not like our server or agree on how we think. We have rules, and people have to abide by them. We the community hope you enjoy your stay!');
+    	message.reply('Project San Andreas is a RP server for Five M! The Owner is N. Henshaw AKA Cuzco. The owner started Project San Andreas in 2018, and has been working on the server every since. Project San Andreas is a very strong community when brought together! We help everyone learn how to RP and make people feel at home! We give everyone a chance on our community. We are the community of forgiveness. We can not help that some people do not like our server or agree on how we think. We have rules, and people have to abide by them. We the community hope you enjoy your stay!');
   	}
 });
 
