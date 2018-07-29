32cedd941fb65f262e15ae7f0e2d8bcd3be1b024
FunTimesRP <37424473+FunTimesRP@users.noreply.github.com>
2018-07-29 18:15:37 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,35 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '$paintjobs') {
+    	message.reply('#five-m-specials');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '$website') {
+    	message.reply('https://funtimesgamingdevelopmentsupport.mistforums.com/');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '$help') {
+    	message.reply('Commands: $fivem, $paintjobs, $website, $hello, ping. If you need further help go here ---> #❔support❔');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '$hello') {
+    	message.reply('Hi I am the Owners Assistant. If you need any help type $help');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
