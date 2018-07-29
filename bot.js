fdf1b041a7ad4c78f17f45dedfa32165a3c4fbed
FunTimesRP <37424473+FunTimesRP@users.noreply.github.com>
2018-07-29 17:35:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,29 +11,5 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-    if (message.content === '$paintjobs') {
-    	message.reply('go to #five-m-specials for the paintjobs');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === '$hello') {
-    	message.reply('Hi Welcome to FunTimes Gaming Development Support I am the Owners Assistant! If you need help use command $help');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === '$help') {
-    	message.reply('List Of Commands: $fivem, $paintjobs, $backgroundinfo, and $hello! For further help go here --> #❔support❔');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === '$backgroundinfo') {
-    	message.reply('FunTimes Gaming Development Support is a support company for Five M! The Owner is [FT] NiceGuy. The owner started FunTimes Gaming Development Support in 2018, and has been working on the company every since. FunTimes Gaming Development Support is a very helpful compan! We help everyone with making the best FiveM RP sevrer and making people feel satisfied with are work! We work are best to fix everyone server or get the server started on how they want it to be. We the company hope you enjoy your stay');
-  	}
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
