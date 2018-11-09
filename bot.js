6656147c65ff0a556587649439510e25b89282cc
FunTimesRP <37424473+FunTimesRP@users.noreply.github.com>
2018-11-09 16:03:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === '$web') {
+    if (message.content === '/web') {
     	message.reply('https://bubble.is/site/funtimesnetworks/');
   	}
 });
